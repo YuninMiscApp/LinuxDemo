48d7c4624f19da904381b4f8f08a296e855e912c
liming.xie <liming.xie@gmail.com>
2018-10-06 11:14:29 +0800

    project init.

---
+++ tests/test_file.c
@@ -0,0 +1,43 @@
+/**
+  ******************************************************************************
+  * @file test_file.c
+  * @author  leon.xie
+  * @version v1.0.0
+  * @date 2018-10-6 11:10:44
+  * @brief  This file provides all the test_file functions. 
+  ******************************************************************************
+  * @attention
+  *
+  * File For Yunin Software Team Only
+  *
+  * Copyright (C), 2017-2027, Yunin Software Team
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+
+#include "file.h"
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+
+int main(int argc, char *argv[])
+{
+	printf("This is The test file.\n");
+	return 0;
+}
+
+
+#ifdef  __cplusplus
+}
+#endif
+
+
---
+++ util/file.h
@@ -0,0 +1,42 @@
+/**
+  ******************************************************************************
+  * @file file.h 
+  * @author leon.xie
+  * @version v1.0.0
+  * @date 2018-10-6 11:10:39
+  * @brief This file contains all the functions prototypes for the BOARD 
+  *  file 
+  ******************************************************************************
+  * @attention
+  *
+  * File For Yunin Software Team Only
+  *
+  * Copyright (C), 2017-2027, Yunin Software Team
+  ******************************************************************************
+  */ 
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+
+#ifndef __FILE_H
+#define __FILE_H
+
+/* Includes ------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+/* Exported typedef ----------------------------------------------------------*/
+/* Exported variables --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif  /* __FILE_H */
+
+
